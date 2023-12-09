19fb13c401aca14c4fbd74b320f2d7334d2ec25c
Antonija Zrno <antonija.zrno@gmx.de>
2023-12-09 17:00:40 +0100

    added functions

--- script.js
+++ script.js
@@ -11,24 +11,91 @@ const generateButton = document.getElementById("button-generate")
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 const numbers = "0123456789"
-const symbols = "!@#$%^&*()_+="
+const symbols = "!@#$%^&*)_+="
 
 function getUppercase() {
     return upperLetters[Math.floor(Math.random() * upperLetters.length)]
 }
-// console.log(getUppercase())
 
 function getLowercase() {
     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)]
 }
-// console.log(getLowercase())
 
 function getNumber() {
     return numbers[Math.floor(Math.random() * numbers.length)]
 }
-// console.log(getNumber())
 
 function getSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
-// console.log(getSymbol())
+
+function generatePassword() {
+    const len = passwordLength.value
+
+    let password = ""
+
+    if (upperCheckbox.checked) {
+        password += getUppercase()
+    }
+
+    if (lowerCheckbox.checked) {
+        password += getLowercase()
+    }
+
+    if (numbersCheckbox.checked) {
+        password += getNumber()
+    }
+
+    if (symbolsCheckbox.checked) {
+        password += getSymbol()
+    }
+
+    for (let i = password.length; i < len; i++) {
+        const x = generateX()
+
+        password += x
+    }
+
+    passwordLabel.innerText = password
+}
+
+function generateX() {
+    const xs = []
+
+    if (upperCheckbox.checked) {
+        xs.push(getUppercase())
+    }
+
+    if (lowerCheckbox.checked) {
+        xs.push(getLowercase())
+    }
+
+    if (numbersCheckbox.checked) {
+        xs.push(getNumber())
+    }
+
+    if (symbolsCheckbox.checked) {
+        xs.push(getSymbol())
+    }
+
+    if (xs.length === 0) return ""
+    return xs[Math.floor(Math.random() * xs.length)]
+}
+
+generateButton.addEventListener("click", generatePassword)
+
+copyButton.addEventListener("click", () => {
+    const textarea = document.createElement("textarea")
+    const password = passwordLabel.innerText
+
+    if (!password) {
+        return
+    }
+
+    textarea.value = password
+    document.body.append(textarea)
+    textarea.select()
+    document.execCommand("copy")
+    textarea.remove()
+    alert("Password copied to clipboard")
+})
